7e0c703cdc3996d186c0bd3cf51984e580936ad6
Leandro Reis <lleandrofr@gmail.com>
2021-02-25 19:35:35 -0300

    exercicio 6

--- exercises/bloco-4-introducao-javascript/script.js
+++ exercises/bloco-4-introducao-javascript/script.js
@@ -1,24 +1,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// let numbersSum = 0;
 
-// for (let index = 0; index < numbers.length; index += 1) {
-//     numbersSum += numbers[index];
-    
 // }
-// let numbersAverage = (numbersSum/numbers.length);
-// console.log(numbersAverage);
-
-// if (numbers[numbers.length-1] > 20){
-//     console.log("Valor maior que 20!")
-// } else {
-//     console.log("Valor menor ou igual a 20!")
-// }
-let maiorNumero = null;
+let oddNumbers = 0;
+let evenNumbers = 0;
+let oddNumbersArray = [];
 
 for (let index = 0; index < numbers.length; index += 1) {
-     if (numbers[index] > maiorNumero ){
-        maiorNumero = numbers[index];
+    if (numbers[index] % 2 !== 0){
+        oddNumbers ++;
+        oddNumbersArray.push(numbers[index]);
+    } else {
+       evenNumbers++;
     }
-}        
-console.log(maiorNumero);
+}
+if (oddNumbers > 0) {
+    console.log(`Existem ${oddNumbers} números ímpares. E eles são: ${oddNumbersArray}`);
+} else {
+    console.log ("Nenhum valor ímpar encontrado");
+}
