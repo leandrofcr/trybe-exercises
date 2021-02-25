67c822736a4121de4191957e3d364af8c6cc7211
Leandro Reis <lleandrofr@gmail.com>
2021-02-25 18:57:35 -0300

    exercicio 4

--- exercises/bloco-4-introducao-javascript/script.js
+++ exercises/bloco-4-introducao-javascript/script.js
@@ -1,10 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let numbersSum = 0;
+// let numbersSum = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    numbersSum += numbers[index];
+// for (let index = 0; index < numbers.length; index += 1) {
+//     numbersSum += numbers[index];
     
+// }
+// let numbersAverage = (numbersSum/numbers.length);
+// console.log(numbersAverage);
+
+if (numbers[numbers.length-1] > 20){
+    console.log("Valor maior que 20!")
+} else {
+    console.log("Valor menor ou igual a 20!")
 }
-let numbersAverage = (numbersSum/numbers.length);
-console.log(numbersAverage);
