099aca85fb2e8d267e2c633d0acecf589583f116
Leandro Reis <lleandrofr@gmail.com>
2021-02-26 18:55:14 -0300

    exercicio 2

--- exercises/bloco-4-introducao-javascript/4.3-logica/script.js
+++ exercises/bloco-4-introducao-javascript/4.3-logica/script.js
@@ -1,11 +1,24 @@
-//Exercicio 1
-let n = 10
+// //Exercicio 1
+// let n = 10
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   inputLine = inputLine + symbol;
+// }
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   console.log(inputLine);
+// }
+
+
+//Exercicio 2
+let n = 5;
 let symbol = '*';
 let inputLine = '';
 
+
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   inputLine = inputLine + symbol;
-}
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  console.log(inputLine);
+  console.log(inputLine + "\n")
+  
 }
