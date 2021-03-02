07c29f2f844c8d11e97da0e617c0e39a02dfb1b6
Leandro Reis <lleandrofr@gmail.com>
2021-03-02 10:28:06 -0300

    exercicio 1 & 2

--- exercises/bloco-4-introducao-javascript/4.4-objetos-funcoes/parte2-functions.js
+++ exercises/bloco-4-introducao-javascript/4.4-objetos-funcoes/parte2-functions.js
@@ -1,11 +1,28 @@
-function verificaPalindrome(palavra) {
-  let arrayLetras = palavra.split("");
-  let isPalindrome = true;
-  for (let index in arrayLetras) {
-    if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
-      isPalindrome = false;
+//EXERCICIO 1
+// function verificaPalindrome(palavra) {
+//   let arrayLetras = palavra.split("");
+//   let isPalindrome = true;
+//   for (let index in arrayLetras) {
+//     if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+//       isPalindrome = false;
+//     }
+//   }
+//   return isPalindrome;
+// }
+// console.log(verificaPalindrome('arara'));
+
+//EXERCICIO 2
+function verificaMaiorNum (array){
+  let maiorNum = null;
+  for (let index = 0; index < array.length -1; index += 1){
+    if  (array[index] > maiorNum){
+      maiorNum = array[index];
+    } else {
+      
     }
-  }
-  return isPalindrome;
 }
-console.log(verificaPalindrome('arara'));
+return maiorNum;
+}
+
+let arrayTeste = [2, 4, 6, 7, 10, 0, -3];
+console.log(verificaMaiorNum(arrayTeste));
