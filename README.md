44d1006644f92f5c70bd01551334e95f2e186085
Leandro Reis <lleandrofr@gmail.com>
2021-03-24 18:53:46 -0300

    README update

--- README.md
+++ README.md
@@ -68,7 +68,7 @@ Trybe is a web development school that has a genuine commitment to the professio
 ##### Bloco 7: JavaScript ES6 & Testes Unitários
 
 - [x] 7-1: _JavaScript ES6 - let, const, arrow functions e template literals_
-- [ ] 7-2: _JavaScript ES6 - Objects_
+- [x] 7-2: _JavaScript ES6 - Objects_
 - [ ] 7-3: _Testes unitários em JavaScript_
 - [ ] 7-4: _Testando em pequenos passos_
 - [ ] 7-5: _[Projeto - JavaScript Testes Unitários]()_
--- exercises/bloco-7-ES6-teste-unitarios/7.2-es6-objects/parte2.js
+++ exercises/bloco-7-ES6-teste-unitarios/7.2-es6-objects/parte2.js
@@ -1,3 +1,4 @@
+ // Exercicio 1 ao 4
 const lesson1 = {
   materia: 'Matemática',
   numeroEstudantes: 20,
@@ -33,5 +34,19 @@ function showKeys(object) {
   console.log(Object.values(object));
 }
 
-const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
-console.log(allLessons);
+// Exercicio 5
+const allLessons = Object.assign({}, {
+  lesson1
+}, {
+  lesson2
+}, {
+  lesson3
+});
+
+// exercicio 6
+function studentCounter() {
+  const lesson1 = allLessons.lesson1.numeroEstudantes;
+  const lesson2 = allLessons.lesson2.numeroEstudantes;
+  const lesson3 = allLessons.lesson3.numeroEstudantes;
+  return console.log(lesson1 + lesson2 + lesson3);
+}
