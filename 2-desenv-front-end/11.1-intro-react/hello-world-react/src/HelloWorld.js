7632778e2b8aac8041bf30f91e173d513a9ef8be
Leandro Reis <lleandrofr@gmail.com>
2021-04-26 15:56:15 -0300

    My first React Class Component :D

---
+++ 2-desenv-front-end/11.1-intro-react/hello-world-react/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render(){
+    return <span>My first React Class Component! :D</span>
+  }
+}
+
+export default HelloWorld;
