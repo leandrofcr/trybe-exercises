e9ad62326974d8aabd66345d25c3b675b9ef61d8
Leandro Reis <lleandrofr@gmail.com>
2021-08-27 18:14:30 -0300

    add exercises 26.4.1

---
+++ 3-desenv-back-end/26-nodejs/26.4-http-express/exercises/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+
+app.listen(3001, ()=> {
+  console.log('App listening port 3001');
+});
