5494d3dc2e6eff7d82882c41c5efc3c06b7f644d
Leandro Reis <lleandrofr@gmail.com>
2021-09-02 16:54:08 -0300

    configure eslint

--- 3-desenv-back-end/27-arquitetura-software/27.1-intro-camada-model/model-example-mysql/models/Author.js
+++ 3-desenv-back-end/27-arquitetura-software/27.1-intro-camada-model/model-example-mysql/models/Author.js
@@ -1,42 +1,44 @@
 const connection = require('./connection');
 
-
-const getNewAuthor = ({id, firstName, middleName, lastName}) => {
+const getNewAuthor = ({
+  id, firstName, middleName, lastName,
+}) => {
   const fullName = [firstName, middleName, lastName]
     .filter((name) => name).join(' ');
-  
+
   return {
     id,
     firstName,
     middleName,
     lastName,
     fullName,
-  }
-}
+  };
+};
 
 const serialize = (authorData) => {
+  // eslint-disable-next-line
   const { id, first_name, middle_name, last_name } = authorData;
   return {
-    id: id,
+    id,
     firstName: first_name,
     middleName: middle_name,
-    lastName: last_name
-  }
-}
+    lastName: last_name,
+  };
+};
 
-const getAll =  async () => {
+const getAll = async () => {
   const [authors] = await connection.execute(
-    'SELECT id, first_name, middle_name, last_name from authors'
+    'SELECT id, first_name, middle_name, last_name from authors',
   );
   return authors.map(serialize).map(getNewAuthor);
-}
+};
 
 const findById = async (id) => {
   const [authors] = await connection.execute(
     'SELECT id, first_name, middle_name, last_name FROM authors WHERE id=?',
-    [id]
+    [id],
   );
-  if (authors.length === 0 ) return null;
+  if (authors.length === 0) return null;
 
   const { firstName, middleName, lastName } = authors.map(serialize)[0];
 
@@ -44,27 +46,27 @@ const findById = async (id) => {
     id,
     firstName,
     middleName,
-    lastName
-  })
-}
+    lastName,
+  });
+};
 
 const isValid = (firstName, middleName, lastName) => {
-  if(!firstName || typeof firstName !== 'string') return false;
-  if(!lastName || typeof lastName !== 'string') return false;
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
 
   return true;
-}
+};
 
-const create = async (firstName, middleName, lastName ) => {
+const create = async (firstName, middleName, lastName) => {
   connection.execute(
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
-    [firstName, middleName, lastName]
-  )
+    [firstName, middleName, lastName],
+  );
 };
 
 module.exports = {
   getAll,
   findById,
   isValid,
-  create
+  create,
 };
